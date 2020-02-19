81882b2457442cb171aecfb428de9f44d3b726fd
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-02-18 20:17:38 -0800

    added: siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     author: `盆地デベロッパー`,
     description: `バンクーバーのウェブ系カレッジで一から ウェブを学ぶ人。
     現地の生活や、テック系や、 趣味を発信しようと思います`,
-    // siteUrl: ``,
+    siteUrl: `ttps://van-hunter.com/`,
     social: {
       twitter: `EndTaichi`,
       instagram: `nabagibasan`,
