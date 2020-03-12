29257488061cc81c6b148d20c6fbce403493455d
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-03-11 22:31:16 -0700

    fixed:font

--- src/html.js
+++ src/html.js
@@ -11,6 +11,10 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link
+          href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700&display=swap&subset=japanese"
+          rel="stylesheet"
+        ></link>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
